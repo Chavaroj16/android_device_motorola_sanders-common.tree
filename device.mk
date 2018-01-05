42d9e703b8cadae65ad7d5ab9a309b54bd1e3f42
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2018-01-04 23:04:51 -0500

    sanders: Add camera & thermal configs

--- device.mk
+++ device.mk
@@ -16,10 +16,24 @@
 # Inherit from msm8953-common
 $(call inherit-product, device/motorola/msm8953-common/msm8953.mk)
 
+# Camera
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/camera/imx219_chromatix.xml:system/etc/camera/imx219_chromatix.xml \
+    $(LOCAL_PATH)/configs/camera/mot_imx258_bear_chromatix.xml:system/etc/camera/mot_imx258_bear_chromatix.xml \
+    $(LOCAL_PATH)/configs/camera/mot_imx258_mono_chromatix.xml:system/etc/camera/mot_imx258_mono_chromatix.xml \
+    $(LOCAL_PATH)/configs/camera/mot_s5k3l8_bear_chromatix.xml:system/etc/camera/mot_s5k3l8_bear_chromatix.xml \
+    $(LOCAL_PATH)/configs/camera/mot_s5k3l8_mono_chromatix.xml:system/etc/camera/mot_s5k3l8_mono_chromatix.xml \
+    $(LOCAL_PATH)/configs/camera/msm8953_mot_sanders_camera.xml:system/etc/camera/msm8953_mot_sanders_camera.xml \
+    $(LOCAL_PATH)/configs/camera/s5k4h8_chromatix.xml:system/etc/camera/s5k4h8_chromatix.xml
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.target.rc
 
 # Screen density
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+# Thermal
+PRODUCTS_COPY_FILES += \
+    $(LOCAL_FILES)/configs/thermal-engine-sanders.conf:system/etc/thermal-engine-sanders.conf
 
